67277720c3c2e7e0e04c5bf8b4ff93e9e4bced24
preet-kang <kang.preet0511@gmail.com>
2017-07-13 21:05:35 -0400

    Add files via upload

---
+++ MyContactsViewController.h
@@ -0,0 +1,12 @@
+//
+//  MyContactsViewController.h
+//  MyContacts
+//
+//  Created by Charles Konkol on 11/10/13.
+//  Copyright (c) 2013 Chuck Konkol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface MyContactsViewController : UITableViewController
+@property (strong) NSMutableArray *contactarray;
+@end
---
+++ README.md
@@ -0,0 +1,8 @@
+MyContacts-CoreData
+===================
+
+Save, Update, Delete Tutorial CoreData
+
+Get Tutorial: MyContacts database.pdf (In Repo)
+
+Podcast: http://somup.com/cIXXeDVNK
